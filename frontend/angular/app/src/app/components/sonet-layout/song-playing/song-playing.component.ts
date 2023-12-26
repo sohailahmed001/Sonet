08e6113653b00577f4fe6a2950a92fc963e9286c
sohail ahmed <asohail180@gmail.com>
2023-12-26 14:08:23 +0530

    Audio seek/slider changes

--- frontend/angular/app/src/app/components/sonet-layout/song-playing/song-playing.component.ts
+++ frontend/angular/app/src/app/components/sonet-layout/song-playing/song-playing.component.ts
@@ -1,19 +1,28 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-song-playing',
   templateUrl: './song-playing.component.html',
   styleUrls: ['./song-playing.component.scss']
 })
-export class SongPlayingComponent implements OnInit {
+export class SongPlayingComponent implements OnInit, AfterViewInit {
   @ViewChild('audioPlayer') audioPlayerRef: ElementRef;
   audioSamplePath: string = 'assets/sunshine-bliss.mp3' // For Testing
   controlActive: string;
-  sliderValue: number = 10;
+  volumeValue: number = 20;
+  audioDuration = 0;
+  currentTrackTime = 0;
+  isSeeking = false;
 
   constructor() {}
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.audioPlayerRef.nativeElement.addEventListener('loadedmetadata', () => {
+      this.audioDuration = this.audioPlayerRef.nativeElement.duration;
+    });
   }
 
   play() {
@@ -29,6 +38,26 @@ export class SongPlayingComponent implements OnInit {
   stop() {
     this.audioPlayerRef.nativeElement.pause();
     this.audioPlayerRef.nativeElement.currentTime = 0;
+    this.currentTrackTime = 0;
     this.controlActive = 'stop';
+  }
+
+  updateSlider() {
+    if (!this.isSeeking) {
+      this.currentTrackTime = this.audioPlayerRef.nativeElement.currentTime;
+    }
+  }
+
+  seekAudio(event: any) {
+    console.log('Seek', event)
+    this.isSeeking = true;
+    const targetTime = event.value;
+    this.audioPlayerRef.nativeElement.currentTime = targetTime;
+    this.currentTrackTime = targetTime;
+  }
+
+  onSeekEnd(event: any) {
+    console.log('End', event)
+    this.isSeeking = false;
   }
 }
